ba040a8b58a7b88bd2febfc52bdebeaa186af062
mo-tiger <e.rooter@gmail.com>
2022-12-06 17:40:40 +0200

    add floating acctiob button

--- lib/widgets/notes_list_view_pilder.dart
+++ lib/widgets/notes_list_view_pilder.dart
@@ -4,12 +4,11 @@ import 'custom_note_item.dart';
 
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
-
   @override
   Widget build(BuildContext context) {
     return ListView.builder(itemBuilder: (context, index) {
       return const Padding(
-        padding: EdgeInsets.symmetric(vertical: 16),
+        padding: EdgeInsets.symmetric(vertical: 8),
         child: NoteItem(),
       );
     });
