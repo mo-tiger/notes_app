3a8b9cc479a0848f552010a70cac7d68b6d3d45d
mo-tiger <e.rooter@gmail.com>
2022-12-07 16:39:32 +0200

    create edit note view

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,42 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:noteapp/widgets/edit_notes_view_body.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(bottom: 16, top: 16),
-      decoration: BoxDecoration(
-        color: Colors.orange,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: const [
-          ListTile(
-            title: Padding(
-              padding: EdgeInsets.only(bottom: 16),
-              child: Text(
-                "Flutter Tips",
-                style: TextStyle(fontSize: 26),
+    return GestureDetector(
+      onTap: (() {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNotesViewBody();
+        }));
+      }),
+      child: Container(
+        padding: const EdgeInsets.only(bottom: 16, top: 16),
+        decoration: BoxDecoration(
+          color: Colors.orange,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: const [
+            ListTile(
+              title: Padding(
+                padding: EdgeInsets.only(bottom: 16),
+                child: Text(
+                  "Flutter Tips",
+                  style: TextStyle(fontSize: 26),
+                ),
+              ),
+              subtitle: Text("Fight Fight Fight And dance with the d3v!l",
+                  style: TextStyle(fontSize: 16)),
+              trailing: Icon(
+                Icons.delete,
+                color: Colors.black,
               ),
             ),
-            subtitle: Text("Fight Fight Fight And dance with the d3v!l",
-                style: TextStyle(fontSize: 16)),
-            trailing: Icon(
-              Icons.delete,
-              color: Colors.black,
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                "May",
+                style: TextStyle(color: Colors.black, fontSize: 16),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              "May",
-              style: TextStyle(color: Colors.black, fontSize: 16),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
