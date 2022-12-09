d02795e96503aa01f54fea087180ff9702b45434
mo-tiger <e.rooter@gmail.com>
2022-12-09 14:27:09 +0200

    taking input and valedate date

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,28 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:noteapp/view/custom_textfield.dart';
+import 'package:noteapp/widgets/custom_button.dart';
 
 class AddNoteBottomSheat extends StatelessWidget {
   const AddNoteBottomSheat({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: AddNoteForm(),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
       child: Column(
-        children: const [
-          SizedBox(
+        children: [
+          const SizedBox(
             height: 32,
           ),
           CustomTextField(
+            onSaved: (value) {
+              title = value;
+            },
             hint: 'Title',
           ),
-          SizedBox(
+          const SizedBox(
             height: 20,
           ),
           CustomTextField(
+            onSaved: (value) {
+              subTitle = value;
+            },
             hint: 'Content',
             maxLines: 10,
           ),
+          const SizedBox(
+            height: 10,
+          ),
+          CustomButton(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          )
         ],
       ),
     );
